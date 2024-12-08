6c876b5b2bda914652719c23a161371ac87db968
LinoyPeerProject <linoypeer1@gmail.com>
2024-12-08 16:08:02 +0200

    SAVING IMAGE AND NAME EDIT USER

--- users/routes/usersRestController.js
+++ users/routes/usersRestController.js
@@ -134,30 +134,21 @@ router.delete('/:id', auth, async (req, res) => {
 // });
 
 
-// קוד ל- PUT במידה וצריך לעדכן את שם המשתמש
 router.put('/:id', auth, upload.single('image'), async (req, res) => {
     try {
         const { id } = req.params;
-        const userInfo = req.user;  // המידע של המשתמש המחובר
-        const { firstName, middleName, lastName } = req.body;  // נתונים חדשים
+        const userInfo = req.user;
+        const { firstName, middleName, lastName } = req.body;
 
-        console.log('req.body:', req.body);  // מודפסת תכולת req.body
-        console.log('req.file:', req.file);  // מודפסת פרטי התמונה
-
-        // אם לא מדובר במשתמש הנוכחי או אין למשתמש הרשאות אדמין, מחזירים 403
         if (userInfo._id !== id && !userInfo.isAdmin) {
             return res.status(403).send('You do not have permission to edit this user.');
         }
-
-        // יצירת אובייקט עם נתונים מעודכנים
         const updatedData = {};
 
-        // כאן אנחנו מעדכנים את השם בתוך המפתח 'name' ולא ישירות
         if (firstName) updatedData.name = { ...updatedData.name, first: firstName };
         if (middleName) updatedData.name = { ...updatedData.name, middle: middleName };
         if (lastName) updatedData.name = { ...updatedData.name, last: lastName };
 
-        // אם הועלתה תמונה, יש לעדכן את הנתיב שלה
         if (req.file) {
             updatedData.image = {
                 path: req.file.path,
@@ -167,14 +158,12 @@ router.put('/:id', auth, upload.single('image'), async (req, res) => {
             console.log('Image path: ', updatedData.image.path);
         }
 
-        // עדכון הנתונים בבסיס הנתונים
         const userToUpdate = await editUser(id, updatedData);
 
-        // מחזירים את המידע המעודכן של המשתמש
         res.send(userToUpdate);
     } catch (e) {
         console.error(e);
-        res.status(400).send(e.message);  // טיפול בשגיאות
+        res.status(400).send(e.message);
     }
 });
 
