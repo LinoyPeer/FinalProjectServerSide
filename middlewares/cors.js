c5ada6f27e856a94be6c204d3f592ca2d89198ea
LinoyPeerProject <linoypeer1@gmail.com>
2025-01-29 22:01:54 +0200

    cors policy changes

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,16 +1,24 @@
 const cors = require("cors");
 
+// const corsMiddleware = cors({
+//     origin: [
+//         "http://127.0.0.1:5500",
+//         "http://localhost:5500",
+//         "http://127.0.0.1:5173",
+//         "http://localhost:5173",
+//         "http://localhost:5174",
+//         "http://127.0.0.1:5174",
+//         "http://localhost:8181",
+//         "https://finalprojectclientside.onrender.com",
+//     ],
+// });
+
 const corsMiddleware = cors({
-    origin: [
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-        "http://localhost:8181",
-        "https://finalprojectclientside.onrender.com",
-    ],
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: "*",
+    credentials: true,
 });
+
 
 module.exports = corsMiddleware;
